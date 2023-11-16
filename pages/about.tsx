99da2722195c6479053d527bc4798c52ad246fc2
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-11-16 10:54:24 +0200

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,7 @@
-import { title, subtitle } from "@/components/primitives";
+import {title, subtitle} from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import {Card, CardBody, CardHeader} from "@nextui-org/card";
+import {Divider, Listbox, ListboxItem} from "@nextui-org/react";
 
 export default function AboutPage() {
     return (
@@ -7,12 +9,31 @@ export default function AboutPage() {
             <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
                 <div className="inline-block max-w-lg text-center justify-center">
                     <h1 className={title()}>About &nbsp;</h1>
-                    <h1 className={title({ color: "violet" })}>Page&nbsp;</h1>
+                    <h1 className={title({color: "violet"})}>Page&nbsp;</h1>
 
 
-                    <h4 className={subtitle({ class: "mt-4" })}>
-                        Here we`&apos;`ll talk about us
+                    <h4 className={subtitle({class: "mt-4"})}>
+                        We&apos;re simple yet efficient currency converter.
                     </h4>
+
+                    <Card>
+                        <CardHeader>
+                            Tech Stack
+                        </CardHeader>
+                        <CardBody>
+                            <Divider />
+                            <Listbox
+                                aria-label="Tech Stack"
+                            >
+                                <ListboxItem key="Next 13">Next JS 13</ListboxItem>
+                                <ListboxItem key="Tailwind">TailwindCSS</ListboxItem>
+                                <ListboxItem key="Next UI">Next UI</ListboxItem>
+                                <ListboxItem key="Tanstack Query">Tanstack Query</ListboxItem>
+                                <ListboxItem key="Vercel">Hosted @Vercel</ListboxItem>
+                            </Listbox>
+                        </CardBody>
+                    </Card>
+
                 </div>
 
             </section>
