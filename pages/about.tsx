434b7ab8f7adf6458a1b3ee82a521ec398d5211f
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-11-16 23:53:22 +0200

    translate about us page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,23 +3,25 @@ import DefaultLayout from "@/layouts/default";
 import {Card, CardBody, CardHeader} from "@nextui-org/card";
 import {Divider, Listbox, ListboxItem} from "@nextui-org/react";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {useTranslation} from "next-i18next";
 
 export default function AboutPage() {
+    const {t} = useTranslation();
     return (
         <DefaultLayout>
             <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
                 <div className="inline-block max-w-lg text-center justify-center">
-                    <h1 className={title()}>About &nbsp;</h1>
-                    <h1 className={title({color: "violet"})}>Page&nbsp;</h1>
+                    <h1 className={title()}>{t('aboutPage.titleFirst')} &nbsp;</h1>
+                    <h1 className={title({color: "violet"})}>{t('aboutPage.titleSecond')}&nbsp;</h1>
 
 
                     <h2 className={subtitle({class: "mt-4"})}>
-                        We&apos;re simple yet efficient currency converter.
+                        {t('aboutPage.subHeading')}
                     </h2>
 
                     <Card>
                         <CardHeader>
-                            Tech Stack
+                            {t('aboutPage.techStack')}:
                         </CardHeader>
                         <CardBody>
                             <Divider />
